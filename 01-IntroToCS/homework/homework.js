68655efad2cc207e17ccfeb6046b38817e3b6c1b
yahacoral <yahairavittoria@gmail.com>
2022-07-18 13:26:25 -0500

    Add homework 01

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,13 +1,38 @@
 'use strict'
 
+//Usando Iteración
+
 function BinarioADecimal(num) {
   // tu codigo aca
+
+  let sum = 0; // resultado decimal
+  let numReverse = num.split('').reverse().join(''); // binario al reves
+
+  //valor binario: 1011
+  //posicion:      3210
+  //indice:        0123
+  //valor decimal = 1 * (2^0) + 1 * (2^1) + 0 * (2^2) + 1 * (2^3) = 11
+
+  for (let i = 0; i < numReverse.length; i++) {
+     sum += numReverse[i] * 2 ** i;
+  }
+  return sum;
   
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
 
+  // let number = num;
+  // let binary = (number % 2).toString();
+  // for (; number > 1; ) {
+  //     number = parseInt(number / 2);
+  //     binary =  (number % 2) + (binary);
+  // }
+
+  let binary = num.toString(2);
+
+  return binary
 }
 
 
